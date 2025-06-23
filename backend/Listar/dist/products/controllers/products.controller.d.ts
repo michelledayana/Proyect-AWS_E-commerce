a96959ea91168d66281258839a112a1b0fa5cd1d
michelledayana <102559251+michelledayana@users.noreply.github.com>
2025-06-23 13:33:15 -0500

    Delete backend/Listar directory

--- backend/Listar/dist/products/controllers/products.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ProductsService } from '../services/products.service';
-import { Product } from '../entities/product.entity';
-export declare class ProductsController {
-    private readonly productsService;
-    constructor(productsService: ProductsService);
-    findAll(): Promise<Product[]>;
-}
--- backend/Listar/dist/products/services/products.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { Product } from '../entities/product.entity';
-export declare class ProductsService {
-    private readonly productRepo;
-    constructor(productRepo: Repository<Product>);
-    findAll(): Promise<Product[]>;
-}
